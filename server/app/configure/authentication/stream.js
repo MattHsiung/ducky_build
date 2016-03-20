ca5f6d195675749200e01836dfbb6b3298fff13c
Jordan Meeker <jordanwmeeker@gmail.com>
2016-03-19 23:08:38 -0400

    switches token to be in name

--- server/app/configure/authentication/stream.js
+++ server/app/configure/authentication/stream.js
@@ -12,15 +12,14 @@ var tokenGenerator = new FirebaseTokenGenerator("Yn8pOTEBHpYwr9I4njqaN6XlFolJGBw
 module.exports = function (app) {
     app.post('/auth/stream', function(req, res) {
 	    console.log('authenticating stream');
-	    var streamUrl = url.parse(req.body.swfurl, true);
-	    var key = streamUrl.query.key;
-
+	    // var streamUrl = url.parse(req.body.swfurl, true);
+	    var key = req.body.name;
 	    ducky.authWithCustomToken(key, function(error, authData) {
 	    	if (error) {
 	    		console.log("Invalid token", error);
 	    		res.sendStatus(500);
 	    	} else {
-	    		console.log("Token is valid, begin stream at", authData.uid);
+	    		console.log("Token is valid, begin stream at www.ducky.com/channels/" + authData.uid);
 	    		res.sendStatus(200);
 	    	}
 	    });
