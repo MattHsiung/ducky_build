085789ee0aa7c1e58bc901207963207ff9fe2ffe
Jordan Meeker <jordanwmeeker@gmail.com>
2016-03-20 13:41:49 -0400

    cleans up all channels ctrl

--- browser/js/channels/channels.controller.js
+++ browser/js/channels/channels.controller.js
@@ -3,11 +3,10 @@ app.controller('ChannelsCtrl', function ($scope, $firebaseObject) {
 
     var activeChannels = [];
     var firebaseRef = $firebaseObject(ref)
+
+    //load up all active channels
     firebaseRef.$loaded()
     .then(function(data){
-      // console.log(converter(data));
-      console.log('HERES FIREBASE DATA', data);
-      console.log('recored', firebaseRef.$id);
       angular.forEach(firebaseRef, function(value, key) {
           activeChannels.push({username: key, files: value});
        });
