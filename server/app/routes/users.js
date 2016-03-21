f0388695affcd8fabebbce5b858c25d6966d9e25
Jordan Meeker <jordanwmeeker@gmail.com>
2016-03-21 14:40:22 -0400

    adds error handling and route to start following user

--- server/app/routes/users.js
+++ server/app/routes/users.js
@@ -7,32 +7,45 @@ var User = mongoose.model('User');
 //get All streamers, search by firebaseId
 router.get('/allStreamers', (req, res, next) => {
     User.find({ firebase: { $exists: true } }).populate('following').sort()
-        .then( streamers => res.json(streamers));
+        .then( streamers => res.json(streamers))
+        .catch( err => next(err))
 })
 
 //get all followers for a specific user
 router.get('/:userid/followers', (req, res, next) => {
     User.find({following: {$in: [req.params.userid]} } ).populate('following')
-    .then(followers => res.json(followers));
+    .then(followers => res.json(followers))
+    .catch( err => next(err))
+})
+
+router.put('/:userid/followers/:userToFollowId', (req, res, next) => {
+    User.findById(req.params.userid)
+        .then(user => {
+            return user.following.push(req.params.userToFollowId).save()
+        })
+        .then(updatedUser => res.status(201).json(updatedUser))
+        .catch( err => next(err))
 })
 
 //get a user by ID
 router.get('/:userid', (req, res, next) => {
     User.findById(req.params.userid).populate('following')
         .then( user => res.json(user))
+        .catch( err => next(err))
 })
 
 //edit a user by ID
 router.put('/:userid', (req, res, next) => {
     User.findByIdAndUpdate(req.params.userId, req.body, {new: true})
     .then( updatedUser => res.status(201).json(updatedUser))
-    .catch( err => next(err));
+    .catch( err => next(err))
 })
 
 //delete a user by ID
 router.delete('/:userid', (req, res, next) => {
     User.findByIdAndRemove(req.params.userid)
-    .then(() => res.sendStatus(401));
+    .then(() => res.sendStatus(401))
+    .catch( err => next(err))
 })
 
 
