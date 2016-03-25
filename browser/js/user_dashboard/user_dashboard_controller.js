47d400b94896b347a237647288225afc57eb54e3
Franklin Lee <BigBoss@Franklins-MacBook-Pro.local>
2016-03-25 15:53:03 -0400

    built out dashboard with all available information

--- browser/js/user_dashboard/user_dashboard_controller.js
+++ browser/js/user_dashboard/user_dashboard_controller.js
@@ -1,14 +1,9 @@
 'use strict';
 var ducky = new Firebase("https://ducky.firebaseio.com/");
 
-app.controller('UserDashCtrl', function ($scope, theFollowersOfUser, $firebaseObject, AuthService) {
-	$scope.theFollowers = theFollowersOfUser;
+app.controller('UserDashCtrl', function ($scope, $firebaseObject, theUser) {
 	$scope.categories = [];
-
-    AuthService.getLoggedInUser()
-        .then(function(user) {
-            $scope.user = user;
-    })
+    $scope.user = theUser;
 
     //populates the categories on scope with the topics
     $firebaseObject(ducky.child("categories")).$loaded()
@@ -18,9 +13,33 @@ app.controller('UserDashCtrl', function ($scope, theFollowersOfUser, $firebaseOb
                     $scope.categories.push(key);
                 }
             }
-    })
+        })
+
+    //get the metadata on the streamer
+    $firebaseObject(ducky.child('channel').child($scope.user.username)).$loaded()
+        .then(function(data) {
+            $scope.userInfo = data;
+        })
+
+    //get all people who is following the streamer
+    $firebaseObject(ducky.child('subscribers')).$loaded()
+        .then(function(data) {
+            var temp = [];
+            for (var key in data) { 
+                if (String(key).indexOf("$") === -1 && data[key][$scope.user.username]) {
+                    temp.push(key);
+                }
+            }
+            return temp;
+        })
+        .then(function(data) {
+            $scope.numOfFollowers = data.join(", ");
+        })
+
 
 	$scope.updateFirebase = function(data) {
         ducky.child('channel').child($scope.user.username).update(data);
+        alert("Your Channel has been updated")
+        //want to redirect eventually to the channel
 	};
 });
