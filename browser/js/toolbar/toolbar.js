5574424b95c366aec4785a0c5f5f245dcbe985aa
Matt Hsiung <xung@musician.com>
2016-03-24 20:01:54 -0400

    editor themes, fiexed width, other smal fixes

---
+++ browser/js/toolbar/toolbar.js
@@ -0,0 +1,20 @@
+app.controller('ToolbarCtrl', function (Editor, $scope, $log) {
+  var editor = Editor;
+  $scope.themes = {
+    monokai:'monokai',
+    chaos:'chaos',
+    cloudsMidnight:'clouds_midnight',
+    terminal:'terminal',
+    vibrantInk:'vibrant_ink',
+    dreamweaver:'dreamweaver',
+    solar:'solarized_light',
+    kuroir:'kuroir',
+    xcode:'xcode'
+  };
+
+  $scope.setTheme=function(theme){
+      console.log(theme)
+      editor.setTheme("ace/theme/"+theme);
+  }
+
+});
